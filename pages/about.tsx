a54be04f33d22543c6e3da085e7ccba69a751135
Mellet Solbakk <mellet89@gmail.com>
2020-01-19 17:13:30 +0100

    Reword about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,19 +9,20 @@ const AboutPageContainer: NextPage = () => {
           <h1>About</h1>
           <h2>The story</h2>
           <p>
-            CS:GO nades was created in 2016 with the goal to gather a large
-            collection of nades, that can be found with very little effort.
+            CSGONades was created in 2016 to collect a large collection of
+            grenades that can be found with ease.
           </p>
           <p>
-            I am sNipn, I am an avid CS:GO player and a computer science
-            student. I wanted to be able to find new smokes and teach them to my
-            teamates in a simple way so we could wreck noobs in CS:GO.
+            I am Mellet and I am an avid Counter-Strike player and a computer
+            science student. During my matchmaking games, I wanted to be able to
+            find new smokes and teach them to my teammates in a simple way.
           </p>
           <p>
-            As a team, we frequently play with new teammates and sometimes
-            someone does not know how to throw a smoke. Now, with just a link to
-            the nade, they can learn it in a few seconds between rounds and we
-            can get on with our game.
+            With my friends, we regularly rotate in new teammates and it often
+            happens that someone does not know how to throw a nade that we need
+            to execute a strategy for a bomb site. Now, with just a link to the
+            nade, they can learn it in a few seconds between rounds and we can
+            get on with our game.
           </p>
           <h2>My vision</h2>
           <p>
@@ -29,27 +30,18 @@ const AboutPageContainer: NextPage = () => {
             players.
           </p>
           <p>
-            If you are new to CS:GO, this site can help you learn the basic
-            grenades that every player should know. Try to blind your enemies
-            with an easy popflash, block off their vision with a nice smoke or
-            set them on fire with a quick molotov. Gain an edge over your
-            opponents, teach new grenades to your teammates and watch yourself
-            rise through the ranks.
+            If you are new to Counter-Strike, this site can help you learn the
+            basic grenades that every player should know. Try to blind your
+            enemies with an easy pop flash, block off their vision with a nice
+            smoke or set them on fire with a quick molotov.
           </p>
           <p>
-            If you are already good at the game , try to coordinate a smoke
-            execute onto a bombsite together with your teammates or invent your
-            own strategies with the use of some grenades.
+            Gain an edge over your opponents, teach new grenades to your
+            teammates and watch yourself rise through the ranks. If you are
+            already good at the game, try to coordinate a smoke execute onto a
+            bombsite together with your teammates or invent your strategies with
+            the use of some of the grenades you can find on this website.
           </p>
-          <h2>Key features</h2>
-          <ul>
-            <li>Short video showing how to throw each nade.</li>
-            <li>Images depicting how to line up your crosshair.</li>
-            <li>
-              In-game teleportation by copying the teleport-code into the
-              console. (Not available for all nades)
-            </li>
-          </ul>
         </div>
       </Layout>
       <style jsx>{`
