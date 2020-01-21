89507c520ee6c167588df4c755cb5e1553d11309
Mellet Solbakk <mellet89@gmail.com>
2020-01-21 22:28:06 +0100

    Add new nade guide

--- src/pages/newnade/NewNadePage.tsx
+++ src/pages/newnade/NewNadePage.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Button, Divider, Grid, Segment } from "semantic-ui-react";
+import { Button, Divider, Grid, Message, Segment } from "semantic-ui-react";
 import { useNewNade } from "../../store/NewNadeStore/NewNadeHooks";
 import { Layout } from "../../ui-common/Layout";
 import { AddGfyContainer } from "./GfyModal/AddGfyContainer";
@@ -13,6 +13,19 @@ export const NewNadePage: FC = () => {
   return (
     <Layout title="New nade" canonical="/newnade">
       <div className="nade-new-container">
+        <Message info>
+          <h2>Important</h2>
+          <p>So you want to add a nade. Great!</p>
+          <p>Follow these simple steps and it will look beautiful!</p>
+          <h3>Recording</h3>
+          <p>Hide your HUD while recording the throw:</p>
+          <code>sv_cheats 1; cl_draw_only_deathnotices 1</code>
+          <h3>Screenshot</h3>
+          <p>Hide everything for the screenshot:</p>
+          <code>
+            sv_cheats 1; r_drawviewmodel 0; cl_draw_only_deathnotices 1
+          </code>
+        </Message>
         <h2>Add new nade</h2>
         <Segment placeholder>
           <Grid columns={2} stackable textAlign="center">
