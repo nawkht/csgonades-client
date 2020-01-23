dd8475c794a90ec574c1f2ae234d5d0d523ba702
Mellet Solbakk <mellet89@gmail.com>
2020-01-23 01:49:58 +0100

    Buggy, fix later

--- pages/maps.tsx
+++ pages/maps.tsx
@@ -1,7 +1,6 @@
 import { NextPage } from "next";
 import { CsgoMap } from "../src/models/Nade/CsGoMap";
 import { MapPage } from "../src/pages/maps/MapPage";
-import { clearSelectedNadeAction } from "../src/store/NadeStore/NadeActions";
 import { fetchNadesByMapActionThunk } from "../src/store/NadeStore/NadeThunks";
 
 interface Props {
@@ -16,7 +15,6 @@ Map.getInitialProps = async context => {
   const map = context.query.name as CsgoMap;
   const { dispatch } = context.store;
 
-  dispatch(clearSelectedNadeAction());
   await dispatch(fetchNadesByMapActionThunk(map));
 
   return { map };
