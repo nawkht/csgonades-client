1b54f2299edbd149b19cfe6bcee7a1a781233307
Mellet Solbakk <mellet89@gmail.com>
2020-01-23 16:33:34 +0100

    Pretty sign in button

--- src/ui-common/layout-components/SignInnButton.tsx
+++ src/ui-common/layout-components/SignInnButton.tsx
@@ -11,35 +11,73 @@ export const SignInnButton: FC = () => {
   const { uiDimensions } = useTheme();
   return (
     <>
-      <div className="steam-login">
-        <a href={AUTH_URL}>
-          <Icon name="steam" />
-          <span>SIGN IN THROUGH STEAM</span>
+      <div className="steam-login-wrapper">
+        <a className="steam-login" href={AUTH_URL} rel="nofollow">
+          <div className="steam-content">
+            <div className="steam-header">Sign in through STEAM</div>
+            <div className="steam-notice">
+              This site not accosiated with Valve Corp.
+            </div>
+
+            <div className="steam-logo">
+              <Icon name="steam" />
+            </div>
+          </div>
         </a>
       </div>
       <style jsx>{`
-        .steam-login {
+        .steam-login-wrapper {
           display: flex;
           align-items: center;
-          padding-right: 12px;
-          opacity: 0.85;
-          transition: opacity 0.2s;
+        }
+        .steam-login {
+          display: flex;
+          margin-right: 18px;
+          border-radius: 8px;
+          align-items: center;
+          color: white;
+          transition: background 0.15s;
+          overflow: hidden;
+          opacity: 0.95;
         }
 
         .steam-login:hover {
           opacity: 1;
         }
 
-        .steam-login a {
-          display: inline-block;
-          padding: 6px 12px;
-          background: #222;
-          color: white;
-          border-radius: ${uiDimensions.BORDER_RADIUS};
+        .steam-login:hover .steam-logo {
+          transform: scale(1.1);
         }
 
-        .steam-login span {
-          margin-left: 6px;
+        .steam-content {
+          position: relative;
+        }
+
+        .steam-logo {
+          display: flex;
+          align-items: center;
+          position: absolute;
+          right: 0;
+          bottom: 0;
+          top: 0;
+          font-size: 2em;
+          line-height: 1em;
+          padding-right: 6px;
+          transform: scale(1);
+          transition: transform 0.3s;
+          padding-top: 2px;
+        }
+
+        .steam-header {
+          background: rgba(34, 34, 34, 1);
+          padding: 6px 56px 3px 12px;
+        }
+
+        .steam-notice {
+          background: rgba(56, 56, 56, 1);
+          font-size: 0.6em;
+          color: rgba(255, 255, 255, 0.8);
+          padding: 0px 0px 3px 12px;
         }
       `}</style>
     </>
