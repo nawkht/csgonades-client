66573a2998f77eb9dfc9d6a908fe57ae25da1bf8
Mellet Solbakk <mellet89@gmail.com>
2020-01-26 04:41:32 +0100

    Better filter colors on light mode

--- src/pages/maps/MapView/Filters.tsx
+++ src/pages/maps/MapView/Filters.tsx
@@ -62,7 +62,7 @@ export const Filters: FC<Props> = ({ map, onOpenOpen, mapViewIsOpen }) => {
           border-top-right-radius: 4px;
           border-bottom-right-radius: 4px;
           cursor: pointer;
-          color: ${colors.filterColor};
+          color: white;
           transition: background 0.15s;
         }
 
