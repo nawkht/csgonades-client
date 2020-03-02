e2f46f856eee147026f4fa4164b55af4e9b0e5be
Mellet Solbakk <mellet89@gmail.com>
2020-03-03 00:13:53 +0100

    Ad tweak

--- src/common/ads/AdSelectors.tsx
+++ src/common/ads/AdSelectors.tsx
@@ -1,11 +1,23 @@
 import { FC } from "react";
 import AmazonAffiliateAdd from "./AmazonAffiliateAd";
-import { AmazonBounty } from "./AmazonBounty";
 
 type Props = {};
 
 export const AdSelector: FC<Props> = ({}) => {
-  const adComps = [<AmazonAffiliateAdd key="1" />, <AmazonBounty key="2" />];
+  const adComps = [
+    <AmazonAffiliateAdd key="1" />,
+    <AmazonAffiliateAdd key="2" />,
+    <>
+      <a href="https://www.kqzyfj.com/click-8411954-12814544" target="_top">
+        <img
+          src="https://www.tqlkg.com/image-8411954-12814544"
+          width="300"
+          height="250"
+          alt=""
+        />
+      </a>
+    </>,
+  ];
 
   const randomAd = adComps[Math.floor(Math.random() * adComps.length)];
 
