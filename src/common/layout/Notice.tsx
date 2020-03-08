469d429e414bb3a04dc14876bd5e1d3374f1735d
Mellet Solbakk <mellet89@gmail.com>
2020-03-08 07:15:18 +0100

    Update version, packages and notice msg

--- src/common/layout/Notice.tsx
+++ src/common/layout/Notice.tsx
@@ -7,25 +7,29 @@ export const Notice: FC<Props> = ({}) => {
     <>
       <div className="notice">
         <div className="notice-msg">
-          We are currently expecting some issues with our Content Delivery
-          Service. If images are not loading try navigating to a new site and it
-          might fix it.
-          <br />
-          We are working on a fix but it might take a while to take effect.
-          <br />
+          <p>
+            We are currently expecting some issues with our Content Delivery
+            Service.
+            <br />
+            Affected system: Login, image loading, button interaction.
+            <br />
+            We are working on a fix, but it might take a while to take effect.
+          </p>
         </div>
       </div>
       <style jsx>{`
         .notice {
           background: #821109;
         }
-        .notice-msg {
+        .notice-msg p {
           color: white;
-          padding: 10px;
-          max-width: 600px;
+          padding-top: 10px;
+          padding-bottom: 10px;
+          margin-left: 40px;
+          margin-right: 40px;
+          max-width: 1200px;
           margin: 0 auto;
           font-size: 14px;
-          text-align: center;
         }
       `}</style>
     </>
