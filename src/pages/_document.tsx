cd0281f497fabebcf1adcd30602303e903d523fd
Mellet Solbakk <mellet89@gmail.com>
2020-03-08 07:56:16 +0100

    Ezoic test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,6 +37,13 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
           />
+          <script
+            dangerouslySetInnerHTML={{ __html: `var ezoicId = 179726;` }}
+          />
+          <script
+            type="text/javascript"
+            src="//go.ezoic.net/ezoic/ezoic.js"
+          ></script>
         </Head>
         <body>
           <Main />
