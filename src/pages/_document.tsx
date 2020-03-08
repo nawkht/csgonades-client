a092b1930db6ce8d38537e6c78a50305a88b0b82
Mellet Solbakk <mellet89@gmail.com>
2020-03-08 11:43:27 +0100

    Helmet nextjs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,41 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import Helmet from "react-helmet";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
-    return { ...initialProps };
+    return { ...initialProps, helmet: Helmet.renderStatic() };
+  }
+
+  get helmetHtmlAttrComponents() {
+    // @ts-ignore
+    return this.props.helmet.htmlAttributes.toComponent();
+  }
+
+  // should render on <body>
+  get helmetBodyAttrComponents() {
+    // @ts-ignore
+    return this.props.helmet.bodyAttributes.toComponent();
+  }
+
+  get helmetHeadComponents() {
+    return (
+      //@ts-ignore
+      Object.keys(this.props.helmet)
+        .filter(el => el !== "htmlAttributes" && el !== "bodyAttributes")
+        //@ts-ignore
+        .map(el => this.props.helmet[el].toComponent())
+    );
   }
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" {...this.helmetHtmlAttrComponents}>
         <Head>
+          {this.helmetHeadComponents}
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
@@ -39,7 +62,7 @@ class MyDocument extends Document {
             href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
           />
         </Head>
-        <body>
+        <body {...this.helmetBodyAttrComponents}>
           <Main />
           <NextScript />
         </body>
