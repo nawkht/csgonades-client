83ac7ee491225e9d0d0075a3b16601b8f9a07940
Mellet Solbakk <mellet89@gmail.com>
2020-03-09 19:22:45 +0100

    Add lastmod to sitemap

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -21,10 +21,25 @@ function mapsList() {
   return maps;
 }
 
+function createLastMode(date) {
+  let modDate = date;
+  if (typeof modDate === "string") {
+    modDate = new Date(date);
+  }
+
+  const lastMod = `${modDate.getFullYear()}-${(
+    "0" +
+    (modDate.getMonth() + 1)
+  ).slice(-2)}-${("0" + modDate.getDate()).slice(-2)}`;
+  return lastMod;
+}
+
 const SITE_ROOT = "https://www.csgonades.com";
 const API_SOURCE = "https://api.csgonades.com";
 
 const createSitemap = async () => {
+  const now = new Date();
+
   let xml = "";
   xml += '<?xml version="1.0" encoding="UTF-8"?>';
   xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
@@ -53,6 +68,7 @@ const createSitemap = async () => {
     const page = `${SITE_ROOT}/maps/${map}`;
     xml += "<url>";
     xml += `<loc>${page}</loc>`;
+    xml += `<lastmod>${createLastMode(now)}</lastmod>`;
     xml += `<changefreq>always</changefreq>`;
     xml += `<priority>0.5</priority>`;
     xml += "</url>";
@@ -67,6 +83,7 @@ const createSitemap = async () => {
       xml += `${SITE_ROOT}/nades/${nade.id}`;
       xml +=
         "</loc><changefreq>always</changefreq><priority>0.5</priority></url>";
+      xml += `<lastmod>${createLastMode(nade.updatedAt)}</lastmod>`;
     }
   } catch (error) {
     console.error(error);
