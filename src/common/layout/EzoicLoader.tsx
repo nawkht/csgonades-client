c3f288156fe29d603a05943cf5b8b794ca2a8e27
Mellet Solbakk <mellet89@gmail.com>
2020-03-10 21:12:51 +0100

    Other method to avoiz ezoic running multiple times

--- src/common/layout/EzoicLoader.tsx
+++ src/common/layout/EzoicLoader.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useEffect } from "react";
+import { FC, memo, useEffect, useState } from "react";
 
 const isBrowser = typeof window !== "undefined";
 
@@ -7,31 +7,27 @@ type Props = {
 };
 
 export const EzoicLoader: FC<Props> = memo(({ codes }) => {
+  const [hasRun, setHasRun] = useState(false);
   useEffect(() => {
-    const delay = setTimeout(() => {
+    // @ts-ignore
+    if (isBrowser && ezstandalone && !hasRun) {
       // @ts-ignore
-      if (isBrowser && ezstandalone) {
+      if (!ezstandalone.enabled) {
+        console.log("> Ezoic enable");
         // @ts-ignore
-        if (!ezstandalone.enabled) {
-          console.log("> Ezoic enable");
-          // @ts-ignore
-          ezstandalone.define(...codes);
-          // @ts-ignore
-          ezstandalone.enable();
-          // @ts-ignore
-          ezstandalone.display();
-        } else {
-          console.log("> Ezoic refresh");
-          // @ts-ignore
-          ezstandalone.refresh();
-        }
+        ezstandalone.define(...codes);
+        // @ts-ignore
+        ezstandalone.enable();
+        // @ts-ignore
+        ezstandalone.display();
+      } else {
+        console.log("> Ezoic refresh");
+        // @ts-ignore
+        ezstandalone.refresh();
       }
-    }, 1000);
-
-    return () => {
-      clearTimeout(delay);
-    };
-  }, [codes]);
+      setHasRun(true);
+    }
+  }, [codes, hasRun]);
 
   return (
     <>
