af7649ea85bfd044ce16a36a7606f6b1b36f5d84
Mellet Solbakk <mellet89@gmail.com>
2020-03-10 20:37:17 +0100

    Try fixing ezoic

--- src/common/layout/EzoicLoader.tsx
+++ src/common/layout/EzoicLoader.tsx
@@ -9,26 +9,33 @@ export const EzoicLoader: FC<Props> = memo(({}) => {
     const executeEzoic = setTimeout(() => {
       // @ts-ignore
       if (isBrowser && ezstandalone) {
-        console.log("> Ezoic execute");
-        // @ts-ignore
-        ezstandalone.define(102, 101);
         // @ts-ignore
         if (!ezstandalone.enabled) {
           console.log("> Ezoic enable");
           // @ts-ignore
+          ezstandalone.define(102, 101);
+          // @ts-ignore
           ezstandalone.enable();
           // @ts-ignore
           ezstandalone.display();
+          console.log({
+            // @ts-ignore
+            ezstandalone,
+          });
         } else {
           console.log("> Ezoic refresh");
           // @ts-ignore
-          ezstandalone.display();
-          // @ts-ignore
           ezstandalone.refresh();
+          console.log({
+            // @ts-ignore
+            ezstandalone,
+          });
         }
       }
     }, 1000);
-    return () => clearTimeout(executeEzoic);
+    return () => {
+      clearTimeout(executeEzoic);
+    };
   }, []);
 
   return (
