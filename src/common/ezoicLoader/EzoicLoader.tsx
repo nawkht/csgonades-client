82d275b15f3f7b530dd6299a54d2273a565a089f
Mellet Solbakk <mellet89@gmail.com>
2020-03-10 22:48:50 +0100

    Old ezoic

--- src/common/ezoicLoader/EzoicLoader.tsx
+++ src/common/ezoicLoader/EzoicLoader.tsx
@@ -1,38 +1,19 @@
-// @ts-nocheck
 import { FC, memo, useEffect, useState } from "react";
-
-const isBrowser = typeof window !== "undefined";
+import { ezoicInit } from "./EzoinInit";
 
 type Props = {
   codes: number[];
 };
 
 export const EzoicLoader: FC<Props> = memo(({ codes }) => {
-  const [cleanEzoicObject, setCleanEzoicObject] = useState(null);
   const [hasRun, setHasRun] = useState(false);
 
   useEffect(() => {
     if (!hasRun) {
-      if (isBrowser && ezstandalone) {
-        if (ezstandalone) {
-          const copyOriginal = Object.assign({}, ezstandalone);
-          setCleanEzoicObject(copyOriginal);
-        }
-
-        ezstandalone.DEBUG = true;
-        ezstandalone.define(...codes);
-        ezstandalone.enable();
-        ezstandalone.display();
-      }
+      ezoicInit(codes);
       setHasRun(true);
     }
-    return () => {
-      console.log("> Resetting ezoic");
-      if (cleanEzoicObject) {
-        ezstandalone = cleanEzoicObject;
-      }
-    };
-  }, [codes, hasRun, cleanEzoicObject]);
+  }, [codes, hasRun]);
 
   return null;
 });
