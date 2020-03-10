18d85501db48a5bcab1156edb6121e97e8f92fa5
Mellet Solbakk <mellet89@gmail.com>
2020-03-10 22:03:21 +0100

    Ezoic refresh

--- src/common/ezoicLoader/EzoinInit.ts
+++ src/common/ezoicLoader/EzoinInit.ts
@@ -1,17 +1,15 @@
 const isBrowser = typeof window !== "undefined";
 
 export function ezoicInit(codes: number[]) {
-  if (isBrowser && ezstandalone) {
+  if (isBrowser && ezstandalone && !ezstandalone.enabled) {
     console.log("> Ezoic enable");
-    ezstandalone.init();
     ezstandalone.define(...codes);
-    if (!ezstandalone.enabled) {
-      ezstandalone.enable();
-    }
+    ezstandalone.enable();
     ezstandalone.display();
-  } /*else if (isBrowser && ezstandalone && ezstandalone.enabled) {
-    console.log("> Ezoic refresh");
-    ezstandalone.destroy();
-    ezstandalone.refresh();
-  }*/
+  } else if (isBrowser && ezstandalone && ezstandalone.enabled) {
+    setTimeout(() => {
+      console.log("> Ezoic refresh");
+      ezstandalone.refresh();
+    }, 1000);
+  }
 }
