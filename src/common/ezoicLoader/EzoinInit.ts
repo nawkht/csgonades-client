972fc85d4792ded62c42d60353f245d234f983f5
Mellet Solbakk <mellet89@gmail.com>
2020-03-10 23:20:36 +0100

    Try usin PWA ezoic

--- src/common/ezoicLoader/EzoinInit.ts
+++ src/common/ezoicLoader/EzoinInit.ts
@@ -3,6 +3,7 @@ const isBrowser = typeof window !== "undefined";
 export function ezoicInit(codes: number[]) {
   if (isBrowser && ezstandalone && !ezstandalone.enabled) {
     console.log("> Ezoic enable");
+    ezstandalone.setIsPWA();
     ezstandalone.define(...codes);
     ezstandalone.enable();
     ezstandalone.display();
@@ -10,7 +11,6 @@ export function ezoicInit(codes: number[]) {
     setTimeout(() => {
       // @ts-ignore
       ezstandalone.cmd.push(function() {
-        console.log("Placeholder", ezstandalone.getPlaceholderElems());
         console.log("> Executing refresh");
         ezstandalone.init();
         ezstandalone.refresh();
