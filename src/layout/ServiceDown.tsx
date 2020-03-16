5454fc76f8a7c4796733051849680e30fd56ad70
Mellet Solbakk <mellet89@gmail.com>
2020-03-16 17:25:10 +0100

    Dynamic service checker

--- src/layout/ServiceDown.tsx
+++ src/layout/ServiceDown.tsx
@@ -1,14 +1,26 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+import Axios from "axios";
 
 type Props = {};
 
 export const ServiceDown: FC<Props> = ({}) => {
+  const [serviceUp, setServiceUp] = useState(true);
+
+  useEffect(() => {
+    Axios.get("https://api.csgonades.com")
+      .then(() => setServiceUp(true))
+      .catch(() => setServiceUp(false));
+  }, []);
+
   return (
     <>
-      <div className="service-down">
-        The service seems to be down at the moment. Refresh the page or come
-        back later.
-      </div>
+      {!serviceUp && (
+        <div className="service-down">
+          The service seems to be down at the moment. Refresh the page or come
+          back later.
+        </div>
+      )}
+
       <style jsx>{`
         .service-down {
           background: maroon;
