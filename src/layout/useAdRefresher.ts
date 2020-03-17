5c0da8d7b47c34189b3c7656978922f139e10174
Mellet Solbakk <mellet89@gmail.com>
2020-03-17 09:12:29 +0100

    Route change wait

--- src/layout/useAdRefresher.ts
+++ src/layout/useAdRefresher.ts
@@ -30,8 +30,8 @@ export const useAdRefresher = () => {
     };
 
     const handleRouteChangeEnd = () => {
-      const ids = findAdCode();
       setTimeout(() => {
+        const ids = findAdCode();
         ezInit(ids);
       }, 1000);
     };
