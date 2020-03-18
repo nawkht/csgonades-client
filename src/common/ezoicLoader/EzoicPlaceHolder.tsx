84838e012de718b224835965027a4a4af9bc1454
Mellet Solbakk <mellet89@gmail.com>
2020-03-18 14:48:42 +0100

    Remove auto center from placeholder

--- src/common/ezoicLoader/EzoicPlaceHolder.tsx
+++ src/common/ezoicLoader/EzoicPlaceHolder.tsx
@@ -14,9 +14,6 @@ export const EzoicPlaceHolder: FC<Props> = memo(({ id, height, width }) => {
         .ez-wrap {
           width: ${width ? `${width}px` : "100%"};
           height: ${height ? `${height}px` : "auto"};
-          display: flex;
-          align-items: center;
-          justify-content: center;
         }
       `}</style>
     </>
