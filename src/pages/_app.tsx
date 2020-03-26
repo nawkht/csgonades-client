f235d9afe469d69db5d7f173280060f74ffea95b
Mellet Solbakk <mellet89@gmail.com>
2020-03-26 22:46:38 +0100

    Package upgrades

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,11 @@ import "semantic-ui-css/semantic.min.css";
 import "react-image-crop/dist/ReactCrop.css";
 import { Layout2 } from "../layout/Layout2";
 import { withRedux } from "../utils/WithRedux";
-import * as Sentry from "@sentry/node";
 
-Sentry.init({
-  dsn: "https://da6e22eac1a8487484fb116b8640e3f8@sentry.io/1871885",
-  enabled: false,
-});
-
-const App = ({ Component, pageProps, err }: any) => {
-  // Workaround for https://github.com/zeit/next.js/issues/8592
-  const modifiedPageProps = { ...pageProps, err };
-
+const App = ({ Component, pageProps }: any) => {
   return (
-    <Layout2 {...modifiedPageProps}>
-      <Component {...modifiedPageProps} />
+    <Layout2 {...pageProps}>
+      <Component {...pageProps} />
     </Layout2>
   );
 };
