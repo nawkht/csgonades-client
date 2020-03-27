44af35df07b8d39a9ca4594512c2dec7d095241a
Mellet Solbakk <mellet89@gmail.com>
2020-03-27 21:55:11 +0100

    Use single placeholder sidebar map page

--- src/maps/MapPageSidebar.tsx
+++ src/maps/MapPageSidebar.tsx
@@ -5,45 +5,19 @@ export const MapPageSideBar: FC = memo(() => {
   return (
     <>
       <div className="sjakt">
-        <div className="ez-160 sticky">
+        <div className="sticky">
           <EzoicPlaceHolder width={160} id={140} />
-        </div>
-        <div className="ez-300 sticky">
-          <EzoicPlaceHolder width={300} id={144} />
         </div>
       </div>
 
       <style jsx>{`
         .sjakt {
           flex: 1;
-          padding-bottom: 100px;
-        }
-
-        .last {
-          padding-bottom: 0;
         }
 
         .sticky {
           position: sticky;
           top: 50px;
-        }
-
-        .ez-160 {
-          display: none;
-        }
-
-        @media only screen and (max-width: 1700px) {
-          .ez-300 {
-            display: none;
-          }
-
-          .ez-160 {
-            display: block;
-          }
-        }
-
-        .hidden {
-          display: none;
         }
       `}</style>
     </>
