e8556d9298811c4a237cc14d1f59fb250b38d14c
Mellet Solbakk <mellet89@gmail.com>
2020-03-28 02:39:14 +0100

    Log ad clicks

--- src/common/ezoicLoader/EzoicPlaceHolder.tsx
+++ src/common/ezoicLoader/EzoicPlaceHolder.tsx
@@ -1,9 +1,24 @@
 import { FC, memo } from "react";
+import { useAnalytics } from "../../utils/Analytics";
 
 type Props = {
   id: number;
 };
 
-export const EzoicPlaceHolder: FC<Props> = memo(({ id }) => (
-  <div id={`ezoic-pub-ad-placeholder-${id}`} />
-));
+export const EzoicPlaceHolder: FC<Props> = memo(({ id }) => {
+  const { event } = useAnalytics();
+
+  function onAdClick() {
+    event({
+      category: "Ad",
+      action: "Click",
+      label: `${id}`,
+    });
+  }
+
+  return (
+    <span onClick={onAdClick}>
+      <div id={`ezoic-pub-ad-placeholder-${id}`} />
+    </span>
+  );
+});
