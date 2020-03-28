b9b6de5161687728835ad86c1368ad1dc8e94f81
Mellet Solbakk <mellet89@gmail.com>
2020-03-28 02:20:31 +0100

    Initial fetch client config

--- src/store/SettingsStore/hooks/useFetchClientConfig.ts
+++ src/store/SettingsStore/hooks/useFetchClientConfig.ts
@@ -7,6 +7,13 @@ export const useFetchClientConfig = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    (async () => {
+      const res = await ClientConfigApi.fetchClientConfig();
+      if (res.isOk()) {
+        dispatch(replaceClientConfigAction(res.value));
+      }
+    })();
+
     const clientConfigFetch = setInterval(async () => {
       const res = await ClientConfigApi.fetchClientConfig();
       if (res.isOk()) {
