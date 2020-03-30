6ca102de3461aecb9918c06e06bc55004f0d3930
Mellet Solbakk <mellet89@gmail.com>
2020-03-31 00:32:08 +0200

    Move script pos

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ class MyDocument extends Document {
       <>
         <Html lang="en">
           <Head>
-            <script src="//www.ezojs.com/ezoic/sa.min.js" />
             <meta
               name="viewport"
               content="initial-scale=1.0, width=device-width"
@@ -49,6 +48,7 @@ class MyDocument extends Document {
               key="google-font-roboto"
               media="all"
             />
+            <script src="//www.ezojs.com/ezoic/sa.min.js" />
           </Head>
           <body>
             <Main />
