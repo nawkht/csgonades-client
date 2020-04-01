f0a07b2964eaba80e4d9e06baaf2c47e9f90dc71
Mellet Solbakk <mellet89@gmail.com>
2020-04-01 21:42:17 +0200

    Log displayed to selected ratio

--- src/layout/useCheckIfAdPresent.ts
+++ src/layout/useCheckIfAdPresent.ts
@@ -1,23 +1,32 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { useAnalytics } from "../utils/Analytics";
 
 export const useCheckIfAdsPresent = () => {
   const { asPath } = useRouter();
+  const { event } = useAnalytics();
 
   useEffect(() => {
     const delayedCheck = setTimeout(() => {
-      getVisiblePlaceholder(asPath);
+      if (!ezstandalone.enabled) {
+        return;
+      }
+      const {
+        selectedPlaceholderCount,
+        displayedAdsCount,
+      } = getVisiblePlaceholder();
+      event({
+        category: "Ad Count",
+        action: `Selected(${selectedPlaceholderCount}) Displayed(${displayedAdsCount})`,
+        label: asPath,
+      });
     }, 15 * 1000);
 
     return () => clearTimeout(delayedCheck);
   }, [asPath]);
 };
 
-function getVisiblePlaceholder(url: string) {
-  if (!ezstandalone.enabled) {
-    return;
-  }
-
+function getVisiblePlaceholder() {
   const placeholderElements: Element[] = [];
   const placeholders = document.querySelectorAll<HTMLDivElement>(
     "[id^=ezoic-pub-ad-placeholder]"
@@ -39,9 +48,5 @@ function getVisiblePlaceholder(url: string) {
     ezstandalone.selectedPlaceholders
   ).length;
 
-  console.log({
-    url,
-    selectedPlaceholderCount,
-    displayedAdsCount,
-  });
+  return { selectedPlaceholderCount, displayedAdsCount };
 }
