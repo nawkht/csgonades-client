6445ed6b3aac60ed9ec59e61f27f065017bf443d
Mellet Solbakk <mellet89@gmail.com>
2020-04-06 16:16:24 +0200

    Remove logs, hard links

--- src/common/ezoicLoader/EzoicPlaceHolder.tsx
+++ src/common/ezoicLoader/EzoicPlaceHolder.tsx
@@ -1,45 +1,13 @@
-import { FC, memo, useState, useEffect, useCallback } from "react";
+import { FC, memo } from "react";
 
 type Props = {
   id: number;
 };
 
-const isBrowser = typeof window !== "undefined";
-
 export const EzoicPlaceHolder: FC<Props> = memo(({ id }) => {
-  const [onClient, setOnClient] = useState(false);
-
-  useEffect(() => {
-    if (isBrowser) {
-      setOnClient(true);
-    }
-  }, []);
-
-  if (!onClient) {
-    return null;
-  }
-
-  return <PurePlaceHolder id={id} />;
+  const placeHolderId = `ezoic-pub-ad-placeholder-${id}`;
+  return <div id={placeHolderId} />;
 });
-
-const PurePlaceHolder: FC<Props> = ({ id }) => {
-  const ref = useCallback(
-    (node: HTMLDivElement) => {
-      if (!node) {
-        return;
-      }
-      const placeHolderId = `ezoic-pub-ad-placeholder-${id}`;
-      const ezoicPlaceholder = document.createElement("div");
-      ezoicPlaceholder.setAttribute("id", placeHolderId);
-      node.appendChild(ezoicPlaceholder);
-    },
-    [id]
-  );
-
-  console.log("> Render", id);
-
-  return <div ref={ref} />;
-};
 
 /*
 const PlaceholderObserver: FC<Props> = memo(({ id }) => {
