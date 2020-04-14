b5619c4c72ff95b8667966e8b88c05e0ef4491d0
Mellet Solbakk <mellet89@gmail.com>
2020-04-14 21:14:41 +0200

    No ad delay

--- src/common/adunits/AdUnit.tsx
+++ src/common/adunits/AdUnit.tsx
@@ -17,12 +17,9 @@ const isBrowser = typeof window !== "undefined";
 export const AdUnit: FC<Props> = memo(({ type }) => {
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
-    const delay = setTimeout(() => {
-      if (isBrowser && !isMobile && IS_PROD) {
-        setMounted(true);
-      }
-    }, 1000);
-    return () => clearTimeout(delay);
+    if (isBrowser && !isMobile && IS_PROD) {
+      setMounted(true);
+    }
   }, []);
 
   if (!mounted) {
