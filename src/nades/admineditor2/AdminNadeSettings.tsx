fa683c840a4340c019909b235bcef9e4c9a7350a
Mellet Solbakk <mellet89@gmail.com>
2020-04-27 14:29:59 +0200

    Tweak admin nade settings

--- src/nades/admineditor2/AdminNadeSettings.tsx
+++ src/nades/admineditor2/AdminNadeSettings.tsx
@@ -42,31 +42,38 @@ export const AdminNadeSettings: FC<Props> = ({ nade, onDismiss }) => {
   }
 
   return (
-    <div>
-      <h3>Nade Status</h3>
-      <StatusEditor
-        status={nade.status}
-        statusInfo={nade.statusInfo}
-        onSave={onStatusSave}
-      />
-      <h3>Force User</h3>
-      <ForceUserSettings onClose={onDismiss} nadeId={nade.id} />
-      <h3>Force Date</h3>
-      <ReactDatePicker
-        selected={createdAt}
-        showYearDropdown
-        showMonthDropdown
-        onChange={newDate => setCreatedAt(newDate)}
-      />
-      <br />
-      <button onClick={onUpdateCreatedAt}>UPDATE YEAR</button>
-      <h3>Delete</h3>
-      <CsgnInput
-        label={`Write "DELETE"`}
-        value={deleteConfimMessage}
-        onChange={setDeleteConfimMessage}
-      />
-      <button onClick={onDelete}>Delete</button>
-    </div>
+    <>
+      <div className="admin-nade-setting">
+        <h3>Nade Status</h3>
+        <StatusEditor
+          status={nade.status}
+          statusInfo={nade.statusInfo}
+          onSave={onStatusSave}
+        />
+        <h3>Force User</h3>
+        <ForceUserSettings onClose={onDismiss} nadeId={nade.id} />
+        <h3>Force Date</h3>
+        <ReactDatePicker
+          selected={createdAt}
+          showYearDropdown
+          showMonthDropdown
+          onChange={(newDate) => setCreatedAt(newDate)}
+        />
+        <br />
+        <button onClick={onUpdateCreatedAt}>UPDATE YEAR</button>
+        <h3>Delete</h3>
+        <CsgnInput
+          label={`Write "DELETE"`}
+          value={deleteConfimMessage}
+          onChange={setDeleteConfimMessage}
+        />
+        <button onClick={onDelete}>Delete</button>
+      </div>
+      <style jsx>{`
+        .admin-nade-setting {
+          width: 50vw;
+        }
+      `}</style>
+    </>
   );
 };
