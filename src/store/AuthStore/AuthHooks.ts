c9342f9cf786c0fb9c472840fb39b5c66bca6b88
Mellet Solbakk <mellet89@gmail.com>
2020-05-01 00:34:44 +0200

    Redurect from auth after 10 sec

--- src/store/AuthStore/AuthHooks.ts
+++ src/store/AuthStore/AuthHooks.ts
@@ -154,6 +154,11 @@ export const useOnSignIn = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // Force redirect if something doesn't happen for 10 seconds
+    const delay = setTimeout(() => {
+      router.push("/", "/");
+    }, 10000);
+
     (async () => {
       const { userDetails, userToken } = await trySignInFunc();
       if (!userDetails || !userToken) {
@@ -172,6 +177,10 @@ export const useOnSignIn = () => {
         router.push("/", "/");
       }
     })();
+
+    return () => {
+      clearTimeout(delay);
+    };
   }, [dispatch, router]);
 };
 
