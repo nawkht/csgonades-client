1a7112836ceef6949555b3437efc8e97cedd4ec1
Mellet Solbakk <mellet89@gmail.com>
2020-05-05 04:16:26 +0200

    fix pagelink

--- src/common/PageLink.tsx
+++ src/common/PageLink.tsx
@@ -5,7 +5,7 @@ export const PageLink = forwardRef<Link, any>(
   ({ children, href, as, ...rest }, ref) => {
     return (
       <Link href={href} as={as}>
-        <a style={{ display: "block" }} ref={ref} {...rest}>
+        <a ref={ref} {...rest}>
           {children}
         </a>
       </Link>
